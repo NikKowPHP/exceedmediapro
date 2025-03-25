869e47c60ba501ecd6b28cc6a1008f6547603944
NikKowPHP <nik.kow@outlook.com>
2025-03-25 11:05:49 +0100

    define font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,10 @@ export default async function LocaleLayout({
     <html lang="en" suppressHydrationWarning className="scroll-smooth">
       <head>
         <link rel="canonical" href={siteUrl} />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet"/>
+
         
         {/* Google Analytics Script */}
         {/* <Script
