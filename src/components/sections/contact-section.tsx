f6b54bcef030454116bed08fa39a8794022f8dd8
NikKowPHP <nik.kow@outlook.com>
2025-03-26 10:54:24 +0100

    fix the bg on the form

--- src/components/sections/contact-section.tsx
+++ src/components/sections/contact-section.tsx
@@ -3,6 +3,26 @@ import { Form } from "../form";
 import { IconBadge } from "../ui/icon-badge";
 
 export const ContactSection = () => {
+  const bgStyles = {
+    flexShrink: 0,
+    opacity: 0.5,
+    backgroundSize: "100% 100%",
+    backgroundImage: "url('/bg-form.svg')",
+  };
+  const parentBgStyles = {
+    WebkitFilter: "blur(50px)",
+    filter: "blur(50px)",
+    flex: "none",
+    height: "328px",
+    left: "calc(50.00000000000002% - 923px / 2)",
+    opacity: 0.7,
+    overflow: "hidden",
+    position: "absolute" as const,
+    top: "calc(50.13774104683198% - 600px / 2)",
+    width: "923px",
+    zIndex: 0,
+  };
+
   return (
     <section
       id="cta-section"
@@ -31,8 +51,11 @@ export const ContactSection = () => {
               </div>
             </div>
           </div>
-
-          <div className="w-full">
+          <div className="w-full relative"></div>
+          <div className="w-full relative">
+            <div style={parentBgStyles} className=" inset-0 ">
+              <div style={bgStyles} className="absolute inset-0 "></div>
+            </div>
             <Form />
           </div>
         </div>
